9c8d19af4b4f79a5ea55d98bc8ec80252ce3d33a
Dirlei Costa <dirleisc@hotmail.com>
2022-04-24 23:45:40 -0300

    add Exercícios

--- modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
+++ modulo1/projeto-lista-js/lista-exercicios-intro-js/exercicios.js
@@ -5,7 +5,6 @@ function soma(num1, num2) {
   // implemente sua lógica aqui
   return num1 + num2
 }
-soma (10 + 20);
 
 // EXERCÍCIO 0B
 function imprimeMensagem() {
@@ -18,69 +17,94 @@ function imprimeMensagem() {
 // EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
 
 // EXERCÍCIO 01
-function calculaAreaRetangulo() {
+function calculaAreaRetangulo( altura, largura) {
   // implemente sua lógica aqui
+ 
+return   altura * largura
 
 }
+calculaAreaRetangulo()
 
 // EXERCÍCIO 02
-function imprimeIdade(atual, inicial) {
+function imprimeIdade(atual,  inicial) {
   // implemente sua lógica aqui
-return atual - inicial;
-}
-imprimeIdade(2022 -1983);
+  return atual - inicial
 
-console.log(imprimeIdade);
+}
+imprimeIdade(2020, 1990);
+
+
 
 
 
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
   // implemente sua lógica aqui
-
+  let peso = 70
+  let altura = 1.70
+  let imc = peso / ( altura * altura)
 }
+calculaIMC(imc)
+
 
 // EXERCÍCIO 04
-function imprimeInformacoesUsuario() {
+function imprimeInformacoesUsuario(nome, idade, email) {
   // implemente sua lógica aqui
   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+  console.log("Meu nome é " +  nome ,  "tenho " + idade, "anos, e o meu e-mail é : " + email )
 
 }
+
+imprimeInformacoesUsuario("Alice", 28, "alice@gmail.com" )
+
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
-
+ console.log("Preto", "Branco", "Verde")
 }
+
+
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
+ string = "maiusculas"
 
 }
+retornaPrimeiroElemento(string.toUppercase)
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
   // implemente sua lógica aqui
+  const soma = custo / valorIngresso;
+  return soma;
 
 }
+let total = soma (3000, 100)
+console.log(total)
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
+  let escreve = "vida"
+  escreve = "amor"
 
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
   // implemente sua lógica aqui
+   array = ["Hello", "Word"]
+  return array
 
 }
+console.log(array[0])
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
   // implemente sua lógica aqui
-
+return array[]
 }
 
 // EXERCÍCIO 11
@@ -104,11 +128,17 @@ function checaRenovacaoRG() {
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
   // implemente sua lógica aqui
+  if (ano % 400 == 0) {
+    return true
+  }else if (ano % 100 ==0){
+    return false
 
 }
+console.log(checaAnoBissexto(2022))
+
 
 // EXERCÍCIO 15
 function checaValidadeInscricaoLabenu() {
-  // implemente sua lógica aqui
+  // implemente sua lógica aqui*/
 
 }
