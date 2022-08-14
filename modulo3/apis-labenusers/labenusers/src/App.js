b6244970f183b353c26a8f1443d3e4462c796b7f
Dirlei Costa <dirleisc@hotmail.com>
2022-08-14 18:58:36 -0300

    excluindo arquivos

---
+++ modulo3/apis-labenusers/labenusers/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import CadastroUsuario from './components/CadastroUsuario';
+import ListUsuaios from './components/ListUsuarios';
+
+function App() {
+  return (
+    <div className="App">
+
+      <CadastroUsuario/>
+      {/* <ListUsuaios/> */}
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo3/apis-labenusers/labenusers/src/components/ListUsuarios.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const ListUsuaios = () =>{
+
+   return(
+      <>
+
+      <h2>Pesquisar por usuários</h2>
+      <input type="text" placeholder='digite o nome:' />
+      <button>Procurar</button>
+      <br/>
+      <button>Voltar ao início</button>
+      </>
+   )
+}
+
+export default ListUsuaios;
